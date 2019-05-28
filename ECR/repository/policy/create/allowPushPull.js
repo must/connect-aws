0d6fe4035a6f8f323a04902450f13d76c15004e6
Mustapha Ben Chaaben <mustapha3892@gmail.com>
2019-05-28 17:54:18 +0200

    Removed unnecessary comments

--- ECR/repository/policy/create/allowPushPull.js
+++ ECR/repository/policy/create/allowPushPull.js
@@ -1,121 +1,36 @@
-/**
- *
- * a sample 'hellow <x>' node. you can use this as a template for actual nodes of
- * your package, just remember to remove it or at least not include it in your
- * 'index.js' platform package config.
- *
- */
-
 const platform = require('connect-platform');
 
-/**
- *
- * lets define a new node for the platform.
- *
- */
 platform.core.node({
-  /**
-   *
-   * it should be accessible via '/test-package/hellow'
-   *
-   */
   path: '/aws/ECR/repository/policy/create/allowPushPull',
 
-  /**
-   *
-   * it should not be publicly accessible by default. note that if you
-   * switch this to 'true', this node will also be accessible via web requests
-   * for people who install your package.
-   *
-   */
   public: false,
 
-  /**
-   *
-   * this is not necessary for non-public nodes. for the public nodes, this will
-   * be the http method through which the node is publicly accessible, so either
-   * 'GET', 'POST', 'PUT' or 'DELETE' (yeah we do not support other http methods).
-   *
-   */
   method: 'GET',
 
-  /**
-   *
-   * the list of inputs for this node, in our case merely
-   * a name we want to say 'hellow' to.
-   *
-   */
   inputs: [
     'repositoryName',
     'awsAccountId',
     'users'
    ],
 
-  /**
-   *
-   * the possible list of outputs for your node. these are
-   * different keys that can get different values, in case of
-   * different scenarios that might happen. in this case,
-   * we have only one scenario in which a 'message' containing
-   * our 'hellow' message will be the output.
-   *
-   */
   outputs: ['policy'],
 
-  /**
-   *
-   * these can be control outputs, i.e. outputs without any accompanying
-   * data. if your node should not return any data, make sure to use
-   * control outputs instead of outputs. these can also be used for exceptional
-   * cases like errors or misconfigurations.
-   *
-   */
   controlOutputs: [],
 
-  /**
-   *
-   * these act as the documentation of your node. make sure to write explicit
-   * and helpful documentation on what does this node do and what each input/output/controlOutput
-   * is.
-   *
-   * you can use html tags in these descriptions. classes 'hl', 'hl-blue' and 'hl-green'
-   * can be used to highlight stuff.
-   */
   hints: {
-    /**
-     *
-     * this is the description of the node in general.
-     *
-     */
     node: 'Creates a push/pull repository <span class="hl-blue">policy</span>.',
 
-    /**
-     *
-     * these are the descriptions of each input value.
-     *
-     */
     inputs: {
       repositoryName: 'The <span class="hl-blue">repositoryName</span> to be created.',
       awsAccountId: 'The <span class="hl-blue">awsAccountId</span> to be used.',
       users: 'The user to be granted permission.'
     },
 
-    /**
-     *
-     * these are the descriptions of possible output values.
-     *
-     */
     outputs: {
       policy: 'The generated policy for the <span class="hl-blue">repositoryName</span>, <span class="hl-blue">awsAccountId</span> and <span class="hl-blue">user</span>.'
     },
 
-    /**
-     *
-     * these are the descriptions of possible control outputs.
-     *
-     */
-    controlOutputs: {
-    }
+    controlOutputs: { }
   }
 },
   (inputs, output, control) => {
--- ECR/repository/policy/set.js
+++ ECR/repository/policy/set.js
@@ -1,128 +1,41 @@
-/**
- *
- * a sample 'hellow <x>' node. you can use this as a template for actual nodes of
- * your package, just remember to remove it or at least not include it in your
- * 'index.js' platform package config.
- *
- */
-
 const platform = require('connect-platform');
 
 const aws = require('../../../connection');
 
-/**
- *
- * lets define a new node for the platform.
- *
- */
 platform.core.node({
-  /**
-   *
-   * it should be accessible via '/test-package/hellow'
-   *
-   */
   path: '/aws/ECR/repository/policy/set',
 
-  /**
-   *
-   * it should not be publicly accessible by default. note that if you
-   * switch this to 'true', this node will also be accessible via web requests
-   * for people who install your package.
-   *
-   */
   public: false,
 
-  /**
-   *
-   * this is not necessary for non-public nodes. for the public nodes, this will
-   * be the http method through which the node is publicly accessible, so either
-   * 'GET', 'POST', 'PUT' or 'DELETE' (yeah we do not support other http methods).
-   *
-   */
   method: 'GET',
 
-  /**
-   *
-   * the list of inputs for this node, in our case merely
-   * a name we want to say 'hellow' to.
-   *
-   */
-  inputs: [
-    'repositoryName',
-    'policy'
-   ],
-
-  /**
-   *
-   * the possible list of outputs for your node. these are
-   * different keys that can get different values, in case of
-   * different scenarios that might happen. in this case,
-   * we have only one scenario in which a 'message' containing
-   * our 'hellow' message will be the output.
-   *
-   */
+  inputs: [ 'repositoryName', 'policy' ],
   outputs: ['repository'],
 
-  /**
-   *
-   * these can be control outputs, i.e. outputs without any accompanying
-   * data. if your node should not return any data, make sure to use
-   * control outputs instead of outputs. these can also be used for exceptional
-   * cases like errors or misconfigurations.
-   *
-   */
   controlOutputs: ['error'],
 
-  /**
-   *
-   * these act as the documentation of your node. make sure to write explicit
-   * and helpful documentation on what does this node do and what each input/output/controlOutput
-   * is.
-   *
-   * you can use html tags in these descriptions. classes 'hl', 'hl-blue' and 'hl-green'
-   * can be used to highlight stuff.
-   */
   hints: {
-    /**
-     *
-     * this is the description of the node in general.
-     *
-     */
     node: 'Sets a <span class="hl-blue">policy</span> to a repository using the <span class="hl-blue">repositoryName</span>. <b>This replaces the old policy!</b>',
 
-    /**
-     *
-     * these are the descriptions of each input value.
-     *
-     */
     inputs: {
       repositoryName: 'The <span class="hl-blue">repositoryName</span> to be created.',
       policy: 'The <span class="hl-blue">policy</span> to be set.'
     },
 
-    /**
-     *
-     * these are the descriptions of possible output values.
-     *
-     */
     outputs: {
       repository: 'the returned result object for the <span class="hl-blue">repository</span>.'
     },
 
-    /**
-     *
-     * these are the descriptions of possible control outputs.
-     *
-     */
     controlOutputs: {
       error: 'This signals that something bad happened with the <span class="hl-blue">request</span>.'
     }
   }
 },
   (inputs, output, control) => {
+    if(typeof inputs.policy == 'object') inputs.policy = JSON.stringify(inputs.policy);
     var params = {
-      policyText: JSON.stringify(inputs.policy), /* required */
-      repositoryName: inputs.repositoryName, /* required */
+      policyText: inputs.policy,
+      repositoryName: inputs.repositoryName,
       force: false
     };
     
